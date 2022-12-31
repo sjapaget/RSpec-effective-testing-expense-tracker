389d02cce1228689466c069139aabd1e67ebfad7
Samuel Paget <sjapaget@gmail.com>
2022-12-31 13:05:15 +0100

    Finished Chapter 5

--- app/api.rb
+++ app/api.rb
@@ -1,6 +1,8 @@
 require 'sinatra/base'
 require 'json'
 
+require_relative 'ledger'
+
 module ExpenseTracker
   class API < Sinatra::Base
 
@@ -12,11 +14,20 @@ module ExpenseTracker
     post '/expenses' do
       expense = JSON.parse(request.body.read)
       result = @ledger.record(expense)
-      JSON.generate('expense_id' => result.expense_id)
+
+      if result.success?
+        JSON.generate('expense_id' => result.expense_id)
+      else
+        status 422
+        JSON.generate('error' => result.error_message)
+      end
     end
 
     get '/expenses/:date' do
-      JSON.generate([])
+      date = params['date']
+      result = @ledger.expenses_on(date)
+
+      JSON.generate(result)
     end
   end
 end
--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -2,7 +2,6 @@ require_relative '../../../app/api'
 require 'rack/test'
 
 module ExpenseTracker
-  RecordResult = Struct.new(:success?, :expense_id, :error_message)
 
   RSpec.describe API do
     include Rack::Test::Methods
@@ -13,7 +12,12 @@ module ExpenseTracker
 
     let(:ledger) { instance_double('ExpenseTracker::Ledger') }
 
+    def parsed
+      JSON.parse(last_response.body)
+    end
+
     describe 'POST /expenses' do
+
       context 'when the expense is successfully recorded' do
         let(:expense) { { 'some' => 'data' } }
 
@@ -25,7 +29,6 @@ module ExpenseTracker
 
         it 'returns the expense id' do
           post '/expenses', JSON.generate(expense)
-          parsed = JSON.parse(last_response.body)
           expect(parsed).to include('expense_id' => 417)
         end
 
@@ -46,14 +49,51 @@ module ExpenseTracker
 
         it 'returns an error message' do
           post '/expenses', JSON.generate(expense)
-
-          parsed = JSON.parse(last_response.body)
           expect(parsed).to include('error' => 'Expense incomplete')
         end
 
         it 'responds with a 422 (Unprocessable entity)' do
-          post '/expenses', JSON.parse(expense)
+          post '/expenses', JSON.generate(expense)
           expect(last_response.status).to eq(422)
+        end
+      end
+    end
+
+    describe 'GET /expenses/:date' do
+      context 'when expenses exist on the given date' do
+        let(:expenses) { [ { 'an' => 'expenses' }, { 'another' => 'expense' } ] }
+
+        before do
+          date = '2022-12-31'
+          allow(ledger).to receive(:expenses_on).with(date).and_return(expenses)
+        end
+
+        it 'returns the expense records as JSON' do
+          get '/expenses/2022-12-31'
+          expect(parsed).to eq(expenses)
+        end
+
+        it 'responds with a 200 (OK)' do
+          get '/expenses/2022-12-31'
+          expect(last_response.status).to eq(200)
+        end
+      end
+
+      context 'when there are no expenses on the given date' do
+        before do
+          allow(ledger).to receive(:expenses_on)
+            .with('2022-12-31')
+            .and_return([])
+        end
+
+        it 'returns an empty array as JSON' do
+          get '/expenses/2022-12-31'
+          expect(parsed).to eq([])
+        end
+
+        it 'responses with  200 (OK)' do
+          get '/expenses/2022-12-31'
+          expect(last_response.status).to eq(200)
         end
       end
     end
